c49bad2d5d67ff2d8f6b78cdc19c3f5be78eb77d
Iruyan_Zak <yuri.kazuna@gmail.com>
2016-02-05 14:30:05 +0900

    本番で使えるように変更

--- hayaoshi.ino
+++ hayaoshi.ino
@@ -14,86 +14,88 @@ byte answer(BuzzerButton*);
 
 class BuzzerButton{
 private:
-  byte input_pin;
-  bool is_pressed;
-  m_secs pressed_time;
+	byte input_pin;
+	bool is_pressed;
+	m_secs pressed_time;
 
 public:
-  byte output_pin;
+	VarSpeedServo servo;
+	bool is_locked;
 
-  BuzzerButton(){}
+	BuzzerButton(){}
 
-  BuzzerButton(byte o_pin, byte i_pin){
-    pinMode(o_pin, OUTPUT);
-    pinMode(i_pin, INPUT_PULLUP);
-    output_pin = o_pin;
-    input_pin = i_pin;
-    is_pressed = false;
-    pressed_time = 0;
-  }
+	BuzzerButton(byte o_pin, byte i_pin){
+		servo.attach(o_pin);
+		servo.write(0, 64, false);
 
-  bool update(){
-    if(digitalRead(input_pin) == LOW){
-      if(is_pressed && millis() - pressed_time > CHATTERING_TIME){
-          answer(this);
-          return true;
-      }
-      if(!is_pressed){  
-        is_pressed = true;
-        pressed_time = millis();
-      }
-    } else {
-      reset();
-    }
-    return false;
-  }  
+		pinMode(i_pin, INPUT);
+		input_pin = i_pin;
 
-  inline void reset(){
-    is_pressed = false;
-    pressed_time = 0;
-  }
+		is_pressed = false;
+		pressed_time = 0;
+		locked = false;
+	}
+
+	bool update(){
+		if(digitalRead(input_pin) == HIGH){
+			if(!locked){
+				if(is_pressed && millis() - pressed_time > CHATTERING_TIME){
+						answer(this);
+						return true;
+				}
+				if(!is_pressed){
+					is_pressed = true;
+					pressed_time = millis();
+				}
+			}
+		} else {
+			reset();
+		}
+		return false;
+	}
+
+	inline void reset(){
+		is_pressed = false;
+		pressed_time = 0;
+		locked = false;
+	}
 };
 
 BuzzerButton* answering_respondent = NULL;
 BuzzerButton buttons[RESPONDENTS];
 byte frame_count;
-VarSpeedServo servo;
 
 byte answer(BuzzerButton* bb){
-  answering_respondent = bb;
-  for(byte i = 0; i < RESPONDENTS; ++i){
-    buttons[i].reset();
-  }
+	answering_respondent = bb;
+	for(byte i = 0; i < RESPONDENTS; ++i){
+		buttons[i].reset();
+		buttons[i].locked = true;
+	}
 
-  servo.write(SERVO_ANGLE, 0, false);
-
-  return 0;
+	bb->servo.write(SERVO_ANGLE, 0, false);
+	return 0;
 }
 
 
 void setup() {
-  pinMode(RESET_PORT, INPUT_PULLUP);
-  servo.attach(52);
-  servo.write(0, 64, false);
-  
-  for(byte i=0; i<RESPONDENTS; ++i){
-    buttons[i] = BuzzerButton(PORT_START + i, PORT_START + RESPONDENTS + i);
-  }
-  frame_count = 0;
+	pinMode(RESET_PORT, INPUT_PULLUP);
+	for(byte i=0; i<RESPONDENTS; ++i){
+		buttons[i] = BuzzerButton(PORT_START + i, PORT_START + RESPONDENTS + i);
+	}
+	frame_count = 0;
 }
 
 void loop() {
-  if(!answering_respondent){
-    for(byte i = frame_count; i < RESPONDENTS + frame_count; ++i){
-      if(buttons[i % RESPONDENTS].update()) break;
-    }
-    frame_count = (frame_count + 1) % RESPONDENTS;
-  } else {
-    digitalWrite(answering_respondent->output_pin, HIGH);
-  }
-  if(digitalRead(RESET_PORT) == LOW){
-    digitalWrite(answering_respondent->output_pin, LOW);
-    answering_respondent = NULL;
-    servo.write(0, 32, false);
-  }
+	if(!answering_respondent){
+		for(byte i = frame_count; i < RESPONDENTS + frame_count; ++i){
+			if(buttons[i % RESPONDENTS].update()) break;
+		}
+		frame_count = (frame_count + 1) % RESPONDENTS;
+	}
+	if(digitalRead(RESET_PORT) == LOW){
+		if(answering_respondent){
+			answering_respondent->servo.write(0, 32, false);
+			answering_respondent = NULL;
+		}
+	}
 }
